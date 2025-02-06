40d4082bcf4b542ece7c4e6ace9880338ebc8136
hlungwani-al <hlungwani.al6@gmail.com>
2025-02-06 13:21:23 +0200

    bootstrap added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'signin', component:SigninComponent},
+    {path: '', redirectTo: 'signin-page', pathMatch: 'full'},
+    {path: '**', redirectTo: 'signin-page', pathMatch: 'full'}
+];
