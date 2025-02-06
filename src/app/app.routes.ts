936fafe65bbf68277ec58c5486970913b0957850
hlungwani-al <hlungwani.al6@gmail.com>
2025-02-06 13:50:53 +0200

    fixed routings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { SigninComponent } from './pages/signin/signin.component';
 
 export const routes: Routes = [
-    {path:'signin', component:SigninComponent},
+    {path:'signin-page', component:SigninComponent},
     {path: '', redirectTo: 'signin-page', pathMatch: 'full'},
     {path: '**', redirectTo: 'signin-page', pathMatch: 'full'}
 ];
--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
