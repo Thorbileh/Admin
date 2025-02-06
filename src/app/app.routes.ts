ba55f7c114c6ea1f8c9829675bd7ef75afca0a14
hlungwani-al <hlungwani.al6@gmail.com>
2025-02-06 16:21:05 +0200

    ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SigninComponent } from './pages/signin/signin.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 export const routes: Routes = [
     {path:'signin-page', component:SigninComponent},
     {path: '', redirectTo: 'signin-page', pathMatch: 'full'},
-    {path: '**', redirectTo: 'signin-page', pathMatch: 'full'}
+    {path: '**', redirectTo: 'signin-page', pathMatch: 'full'},
+    {path: 'side', component:SideBarComponent},
 ];
