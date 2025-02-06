2b9003815085409b8f8440ae5d1120a9c040e5a3
Thorbileh <preciousthobile99@gmail.com>
2025-02-06 13:54:07 +0200

    pulled the main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TransactionsComponent } from './page/transactions/transactions.component';
+// import { TransactionsComponent } from './pages/transactions/transactions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'transaction' , component:TransactionsComponent},
+];
