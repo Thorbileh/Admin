78409849b17a82ff789f1baefb1e5ff08f4c2297
hlungwani-al <hlungwani.al6@gmail.com>
2025-02-07 11:51:17 +0200

    fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
 import { TransactionsComponent } from './page/transactions/transactions.component';
 import { SigninComponent } from './pages/signin/signin.component';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path:'signin-page', component:SigninComponent},
     {path: 'dashboard', component:DashboardComponent},
-    {path: '', redirectTo: 'signin-page', pathMatch: 'full'},
-   
-    {path:'transaction',component:TransactionsComponent}
+    {path:'transaction',component:TransactionsComponent},
+    {path: '', redirectTo: 'signin-page', pathMatch: 'full'}, 
+    {path: '**', redirectTo:'signin-page', pathMatch:'full'}
 ];
