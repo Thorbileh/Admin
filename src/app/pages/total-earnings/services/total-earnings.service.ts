2762000f98d9b9bbc4afead08073d6b102aa97e5
JudasM <judasmohlala2012@gmail.com>
2025-02-07 14:39:53 +0200

    added another mock data

--- src/app/pages/total-earnings/services/total-earnings.service.ts
+++ src/app/pages/total-earnings/services/total-earnings.service.ts
@@ -12,7 +12,8 @@ export class TotalEarningsService {
   getSubscriptions(): Observable<any[]> {
     return of([
       { runner: 'John Doe', type: 'Monthly', proof: 'tanklas.pdf', amount: 49.00, date: '31 Dec 2024' },
-      { runner: 'Jane Doe', type: 'Yearly', proof: 'proof.pdf', amount: 499.00, date: '31 Dec 2024' },
+      { runner: 'Jane Doe', type: 'Yearly', proof: 'proof.pdf', amount: 499.00, date: '21 Nov 2024' },
+      {runner: 'Amukelani Hlungz', type: 'Monthly', proof: 'capitec.pdf', amount: 49.00, date: '13 Jan 2025'}
     ]);
   }
 
@@ -21,6 +22,7 @@ export class TotalEarningsService {
     return of([
       { orderNumber: 'ASFHBHDUKLV', amount: 455.00, fee: 227.50, date: '31 Dec 2024' },
       { orderNumber: 'ASFHBHDUKLV', amount: 920.00, fee: 45.00, date: '31 Dec 2024' },
+      { orderNumber: 'HMTWHWCWAO', amount: 345.00, fee: 45.00, date: '12 Jan 2025' },
     ]);
   }
 }
