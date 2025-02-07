cd8b8a343cc4be06a3e5c4ecae71b45441f1bd9f
hlungwani-al <hlungwani.al6@gmail.com>
2025-02-07 10:25:02 +0200

    dashboard css

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from "../../components/side-bar/side-bar.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [SideBarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
