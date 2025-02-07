85f15a22828fe0ec7e7767d3c7352c96c65459c4
Thorbileh <preciousthobile99@gmail.com>
2025-02-07 13:32:30 +0200

    created the runners page

---
+++ src/app/pages/runners/runners.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-runners',
+  standalone: true,
+  imports: [],
+  templateUrl: './runners.component.html',
+  styleUrl: './runners.component.css'
+})
+export class RunnersComponent {
+
+}
