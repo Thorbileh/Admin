7797a702495ad855b9fe9a266a41dba9b808c1ae
Thorbileh <preciousthobile99@gmail.com>
2025-02-07 12:56:28 +0200

    removed unused import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { TransactionsComponent } from './page/transactions/transactions.component';
 import { SigninComponent } from './pages/signin/signin.component';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { PayRunnerComponent } from './pages/pay-runner/pay-runner.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
