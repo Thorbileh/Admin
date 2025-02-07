da2e060de3e510b9a7d285ac56eba47d3b8c2c6e
hlungwani-al <hlungwani.al6@gmail.com>
2025-02-07 10:59:56 +0200

    fixed aligning cards

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from "../../components/side-bar/side-bar.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SideBarComponent],
+  imports: [RouterLink, SideBarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
