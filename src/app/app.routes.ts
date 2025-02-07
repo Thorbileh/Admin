636abe02bd99373983e4cab4a74e8f8b1dad45e2
hlungwani-al <hlungwani.al6@gmail.com>
2025-02-07 11:53:52 +0200

    fixed routerlinks on side bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { TransactionsComponent } from './page/transactions/transactions.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { OrdersComponent } from './pages/orders/orders.component';
 
 export const routes: Routes = [
     {path:'signin-page', component:SigninComponent},
     {path: 'dashboard', component:DashboardComponent},
     {path:'transaction',component:TransactionsComponent},
+    {path:'orders',component:OrdersComponent},
     {path: '', redirectTo: 'signin-page', pathMatch: 'full'}, 
     {path: '**', redirectTo:'signin-page', pathMatch:'full'}
 ];
