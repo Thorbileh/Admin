6906db1c6b44d4133da5eaa6f219f17261c5efce
Thorbileh <preciousthobile99@gmail.com>
2025-02-07 14:25:50 +0200

    added pending and approved runners tables

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { TransactionsComponent } from './pages/transactions/transactions.compone
 import { SigninComponent } from './pages/signin/signin.component';
 import { PayRunnerComponent } from './pages/pay-runner/pay-runner.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RunnersComponent } from './pages/runners/runners.component';
 
 export const routes: Routes = [
     {path:'signin-page', component:SigninComponent},
     {path: 'dashboard', component:DashboardComponent},
     {path: '', redirectTo: 'signin-page', pathMatch: 'full'}, 
     {path:'transaction',component:TransactionsComponent},
-    {path:'pay-runner',component:PayRunnerComponent}
+    {path:'pay-runner',component:PayRunnerComponent},
+    {path:'runner',component:RunnersComponent}
+
 ];
