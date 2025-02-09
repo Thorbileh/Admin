e201f44a11ba5fe5352bc55674406ca6fdc42bc7
Hlungwani-al <Hlungwani.al6@gmail.com>
2025-02-09 15:28:41 +0200

    profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
 
+  adminEmail:string ="admin@easyerrands.com";
+  constructor(private router: Router){}
+  signOut (){
+    this.router.navigate(['/signin-page'])
+  }
 }
