97f3d0901e3bd631b64fae7997ee2d4ee707f0ad
JudasM <judasmohlala2012@gmail.com>
2025-02-09 11:40:48 +0200

    added the service, and also the messages btween all parties

--- src/app/pages/complaints/services/complaints.service.ts
+++ src/app/pages/complaints/services/complaints.service.ts
@@ -1,19 +1,51 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ComplaintsService {
+  getComplaintList() {
+    return [
+      {
+        date: '05 Jan 2025',
+        disputeId: '000012223',
+        dispute: 'Delayed Delivery',
+        orderNumber: 'ASF6IHBJOLKV',
+        status: 'Closed',
+      },
+      {
+        date: '22 Dec 2024',
+        disputeId: '000012224',
+        dispute: 'Missing Items',
+        orderNumber: 'ASF6IHYOLKP',
+        status: 'Pending',
+      },
+      // more date later though...
+    ];
+  }
 
-  constructor() {}
-
-   // Mock for Complaints. 
-    getComplaints(): Observable<any[]> {
-      return of([
-        {date: '13 Jun 2022', DisputeID: '000012223', Dispute: 'Not Delivered', OrderNumber : 'ASF6IHBIJOLKV', status: 'closed'}
-      ]);
-    }
-  
-
+  getComplaintDetails(disputeId: string) {
+    return {
+      disputeId,
+      orderNumber: 'ASF6IHBJOLKV',
+      buyerName: 'The Glass Guy',
+      disputeTitle: 'Delayed Delivery',
+      description:
+        'Order was delayed, tried calling the runner but he was not answering my calls.',
+      messages: [
+        {
+          sender: 'Jane Doe',
+          time: '1:02 PM',
+          message: 'But I sent on Tuesday',
+        },
+        { sender: 'John Doe', time: '1:30 PM', message: 'When can I expect?' },
+        {
+          sender: 'Jane Doe',
+          time: '1:32 PM',
+          message:
+            'I apologize, I used the wrong address but I managed to fix it.',
+        },
+      ],
+    };
+  }
 }
